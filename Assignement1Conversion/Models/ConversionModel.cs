8291a908d964732e9ab7c47c100464eb037dd719
SujeethMuru <muruganandamsujeeth@gmail.com>
2026-09-16 13:05:06 -0500

    - Added textbox to type in value for conversion with own double data type propety model called InputValue - Added Convert box - Added Reset box

--- Assignement1Conversion/Models/ConversionModel.cs
+++ Assignement1Conversion/Models/ConversionModel.cs
@@ -3,8 +3,8 @@
     public class ConversionModel
     {
         public string ConversionType { get; set; }
-        
         public string ConversionOption { get; set; }
+        public double InputValue { get; set; }
 
     }
 }
